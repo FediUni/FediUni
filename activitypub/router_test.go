75acb68f4ec80b29dbff8016a8eb639866393aa9
Jared Mullin <JaredMullin@protonmail.com>
2021-11-03 13:09:29 +0000

    Add test for getActor for NotImplemented status

---
+++ activitypub/router_test.go
@@ -0,0 +1,23 @@
+package activitypub
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetActor(t *testing.T) {
+	server := httptest.NewServer(newRouter())
+	defer server.Close()
+	resp, err := http.Get(fmt.Sprintf("%s/actor/bendean", server.URL))
+	if err != nil {
+		t.Errorf("getActor(): returned an unexpected err: got %v want %v", err, nil)
+	}
+	defer resp.Body.Close()
+	gotStatus := resp.StatusCode
+	wantStatus := http.StatusNotImplemented
+	if gotStatus != wantStatus {
+		t.Errorf("getActor(): returned an unexpected status: got %v want %v", gotStatus, wantStatus)
+	}
+}
