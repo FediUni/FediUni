b66b212d78963630f325a72d3188c5823d71a83e
Jared Mullin <JaredMullin@protonmail.com>
2022-01-24 17:20:56 +0000

    Add experimental Digest calculation to compare against Mastodon

---
+++ activitypub/validation/digest.go
@@ -0,0 +1,19 @@
+package validation
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	log "github.com/golang/glog"
+	"io/ioutil"
+	"net/http"
+)
+
+func CalculateDigestHeader(req *http.Request) (*http.Request, error) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	h := sha256.Sum256(body)
+	log.Infof("SHA-256=%s", base64.StdEncoding.EncodeToString(h[:]))
+	return req, nil
+}
