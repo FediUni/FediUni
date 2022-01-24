81e0d45b73056c2b87e6eb66161cea03b6d51f65
Jared Mullin <JaredMullin@protonmail.com>
2022-01-24 17:21:53 +0000

    Fix empty body

--- activitypub/validation/digest.go
+++ activitypub/validation/digest.go
@@ -1,6 +1,7 @@
 package validation
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/base64"
 	log "github.com/golang/glog"
@@ -15,5 +16,6 @@ func CalculateDigestHeader(req *http.Request) (*http.Request, error) {
 	}
 	h := sha256.Sum256(body)
 	log.Infof("SHA-256=%s", base64.StdEncoding.EncodeToString(h[:]))
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 	return req, nil
 }
