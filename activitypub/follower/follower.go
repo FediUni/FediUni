6566212e636ac152be0dec68cbbd71bcf0fe6fa1
Jared Mullin <JaredMullin@protonmail.com>
2022-01-25 13:05:54 +0000

    Add handler for Accept activity to add followers

--- activitypub/follower/follower.go
+++ activitypub/follower/follower.go
@@ -3,10 +3,11 @@ package follower
 import (
 	"context"
 	"fmt"
+	"net/url"
+
 	"github.com/go-fed/activity/streams"
 	"github.com/go-fed/activity/streams/vocab"
 	log "github.com/golang/glog"
-	"net/url"
 )
 
 func ParseFollowRequest(ctx context.Context, activity vocab.Type) (vocab.ActivityStreamsFollow, error) {
@@ -24,6 +25,23 @@ func ParseFollowRequest(ctx context.Context, activity vocab.Type) (vocab.Activit
 	}
 	log.Infoln("Successfully resolved Type to ActivityStreamsFollow")
 	return follow, nil
+}
+
+func ParseAcceptActivity(ctx context.Context, activity vocab.Type) (vocab.ActivityStreamsAccept, error) {
+	var accept vocab.ActivityStreamsAccept
+	acceptResolver, err := streams.NewTypeResolver(func(ctx context.Context, a vocab.ActivityStreamsAccept) error {
+		accept = a
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create TypeResolver: got err=%v", err)
+	}
+	err = acceptResolver.Resolve(ctx, activity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve type to Follow activity: got err=%v", err)
+	}
+	log.Infoln("Successfully resolved Type to ActivityStreamsFollow")
+	return accept, nil
 }
 
 func PrepareAcceptActivity(follow vocab.ActivityStreamsFollow, actorID *url.URL) vocab.ActivityStreamsAccept {
