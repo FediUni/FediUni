df001537daf2caef481f6c969e7a15c75aaa866d
Jared Mullin <JaredMullin@protonmail.com>
2022-03-13 17:36:42 +0000

    Fix header settings

--- server/validation/sign.go
+++ server/validation/sign.go
@@ -32,13 +32,19 @@ func SignRequestWithDigest(r *http.Request, url *url.URL, keyID string, privateK
 	preferences := []httpsig.Algorithm{httpsig.RSA_SHA256, httpsig.RSA_SHA512}
 	headersToSign := []string{httpsig.RequestTarget, "host", "date", "digest"}
 	signer, _, err := httpsig.NewSigner(preferences, httpsig.DigestSha256, headersToSign, httpsig.Signature, 0)
-
-	// See: https://github.com/go-fed/httpsig/issues/20
-	// This is because certain implementations such as Plemora rely on the
-	// algorithm name.
-	r.Header["Signature"][0] = strings.Replace(r.Header["Signature"][0], "algorithm=\"hs2019\"", "algorithm=\"rsa-sha256\"", 1)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create signer: got err=%v", err)
 	}
-	return r, signer.SignRequest(privateKey, keyID, r, body)
+	if err := signer.SignRequest(privateKey, keyID, r, body); err != nil {
+		return nil, err
+	}
+	// See: https://github.com/go-fed/httpsig/issues/20
+	// This is because certain implementations such as Plemora rely on the
+	// algorithm name.
+	log.Infoln(r.Header)
+	signature := r.Header.Get("Signature")
+	newSignature := strings.Replace(signature, "algorithm=\"hs2019\"", "algorithm=\"rsa-sha256\"", 1)
+	log.Infof("Changed Signature=%q to Signature=%q", signature, newSignature)
+	r.Header.Set("Signature", newSignature)
+	return r, nil
 }
