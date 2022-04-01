c84dc7219ae9111525e59aadd8010f2cc1a72e52
Jared Mullin <JaredMullin@protonmail.com>
2022-04-01 16:32:04 +0100

    Add test for DereferenceObjectsInOrderedCollection

--- server/client/client_test.go
+++ server/client/client_test.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"encoding/json"
+	"github.com/FediUni/FediUni/server/object"
 	"net/url"
 	"testing"
 	"time"
@@ -247,6 +248,131 @@ func TestAnnounce(t *testing.T) {
 			gotObject, err := streams.Serialize(object)
 			if d := cmp.Diff(gotObject, test.want); d != "" {
 				t.Errorf("client.FetchRemoteObject(): returned a different object: (-want +got) %s", d)
+			}
+		})
+	}
+}
+
+func TestDereferenceOrderedCollection(t *testing.T) {
+	tests := []struct {
+		name  string
+		page  int
+		key   []string
+		value []map[string]interface{}
+		want  map[string]interface{}
+	}{
+		{
+			name: "Test dereference orderedItems with Note",
+			page: 0,
+			key: []string{
+				"https://non-existent-site.com/object/fake-ordered-collection",
+				"https://non-existent-site.com/object/fake-ordered-collection-page-1",
+				"https://non-existent-site.com/object/fake-ordered-collection-page-2",
+				"https://non-existent-site.com/actor/fake-actor",
+				"https://non-existent-site.com/object/fake-note",
+			},
+			value: []map[string]interface{}{
+				{
+					"@context": "https://www.w3.org/ns/activitystreams",
+					"id":       "https://non-existent-site.com/object/fake-ordered-collection",
+					"type":     "OrderedCollection",
+					"first":    "https://non-existent-site.com/object/fake-ordered-collection-page-1",
+				},
+				{
+					"@context":     "https://www.w3.org/ns/activitystreams",
+					"id":           "https://non-existent-site.com/object/fake-ordered-collection-page-1",
+					"type":         "OrderedCollectionPage",
+					"orderedItems": "https://non-existent-site.com/object/fake-note",
+					"next":         "https://non-existent-site.com/object/fake-ordered-collection-page-2",
+				},
+				{
+					"@context":     "https://www.w3.org/ns/activitystreams",
+					"id":           "https://non-existent-site.com/object/fake-ordered-collection-page-2",
+					"type":         "OrderedCollectionPage",
+					"orderedItems": "https://non-existent-site.com/object/fake-note",
+				},
+				{
+					"@context": "https://www.w3.org/ns/activitystreams",
+					"id":       "https://non-existent-site.com/actor/fake-actor",
+					"type":     "Person",
+				},
+				{
+					"@context":     "https://www.w3.org/ns/activitystreams",
+					"id":           "https://non-existent-site.com/object/fake-note",
+					"type":         "Note",
+					"attributedTo": "https://non-existent-site.com/actor/fake-actor",
+				},
+			},
+			want: map[string]interface{}{
+				"@context": "https://www.w3.org/ns/activitystreams",
+				"id":       "https://non-existent-site.com/object/fake-ordered-collection-page-1",
+				"type":     "OrderedCollectionPage",
+				"orderedItems": map[string]interface{}{
+					"id":   "https://non-existent-site.com/object/fake-note",
+					"type": "Note",
+					"attributedTo": map[string]interface{}{
+						"id":   "https://non-existent-site.com/actor/fake-actor",
+						"type": "Person",
+					},
+				},
+				"next": "https://non-existent-site.com/object/fake-ordered-collection-page-2",
+			},
+		},
+	}
+	for _, test := range tests {
+		marshalledOrderedCollection, err := json.Marshal(test.value[0])
+		if err != nil {
+			t.Fatalf("failed to marshal value: got err=%v", err)
+		}
+		page1, err := json.Marshal(test.value[1])
+		if err != nil {
+			t.Fatalf("failed to marshal value: got err=%v", err)
+		}
+		page2, err := json.Marshal(test.value[2])
+		if err != nil {
+			t.Fatalf("failed to marshal value: got err=%v", err)
+		}
+		marshalledActor, err := json.Marshal(test.value[3])
+		if err != nil {
+			t.Fatalf("failed to marshal value: got err=%v", err)
+		}
+		marshalledNote, err := json.Marshal(test.value[4])
+		if err != nil {
+			t.Fatalf("failed to marshal value: got err=%v", err)
+		}
+		t.Run(test.name, func(t *testing.T) {
+			client := &Client{
+				Cache: &TestCache{
+					cache: map[string][]byte{
+						test.key[0]: marshalledOrderedCollection,
+						test.key[1]: page1,
+						test.key[2]: page2,
+						test.key[3]: marshalledActor,
+						test.key[4]: marshalledNote,
+					},
+				},
+			}
+			marshalledCollection, err := streams.ToType(context.Background(), test.value[0])
+			if err != nil {
+				t.Fatalf("Failed to parse OrderedCollection: got err=%v", err)
+			}
+			orderedCollection, err := object.ParseOrderedCollection(context.Background(), marshalledCollection)
+			if err != nil {
+				t.Fatalf("Failed to parse OrderedCollection: got err=%v", err)
+			}
+			page, err := client.DereferenceObjectsInOrderedCollection(context.Background(), orderedCollection, test.page, 0, 3)
+			if err != nil {
+				t.Errorf("DereferenceObjectsInOrderedCollection() returned an unexpected error: got err=%v", err)
+			}
+			var gotPage map[string]interface{}
+			if page != nil {
+				gotPage, err = streams.Serialize(page)
+				if err != nil {
+					t.Errorf("Failed to serialize Got Page: got err=%v", err)
+				}
+			}
+			if d := cmp.Diff(test.want, gotPage); d != "" {
+				t.Errorf("DereferenceObjectsInOrderedCollection() returned an unexpected diff (-want +got): %s", d)
 			}
 		})
 	}
